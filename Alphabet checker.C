c459b9438cf79ed376dafc75b106aa817ffacac6
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2016-10-05 14:31:41 +0530

    Add files via upload

---
+++ Alphabet checker.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include<ctype.h>
+
+
+int main(void)
+ {
+	
+char ch;
+	
+scanf("%c",&ch);
+	
+  if(isalpha(ch)==0)
+	
+   printf("%c is not an alphabet",ch);
+
+  else
+	 
+  printf("%c is  an alphabet",ch);
+
+  return 0;
+
+}
+
