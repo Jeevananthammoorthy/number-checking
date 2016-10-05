fb320f4e925d54f9a40e3aa44bac86f02577de19
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2016-10-05 22:28:13 +0530

    Add files via upload

---
+++ sumofnaturalnumbers.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+   
+int n,temp=0,i;
+ 
+  scanf("%d",&n);
+
+
+if(n!=0)
+{   for(i=n;i>0;i--)
+
+   temp=temp+i;
+
+   printf("%d",temp);
+}
+else
+{
+printf("Enter the non zero number");
+}
+
+	return 0;
+
+}
+
