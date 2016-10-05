1136f98c96dfc075fadc595d0fa85eec964c8184
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2016-10-05 22:23:43 +0530

    Add files via upload

---
+++ Counting digits.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+   
+int n,count=0;
+ 
+  scanf("%d",&n);
+ 
+  while(n!=0)
+  {
+
+       n=n/10;
+
+       count++;
+ 
+  }
+
+   printf("Number of digits in a number is %d",count);
+	
+return 0;
+
+}
+
