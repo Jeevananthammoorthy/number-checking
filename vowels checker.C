57c892d2360504058e0783a9b3caf7c1137e8a25
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2016-10-05 14:17:49 +0530

    Add files via upload

---
+++ vowels checker.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+
+char ch;
+	
+scanf("%c",&ch);
+if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+
+       printf("The character is vowels");
+
+else
+	
+       printf("The character is not a vowels");
+	
+return 0;
+
+}
+
